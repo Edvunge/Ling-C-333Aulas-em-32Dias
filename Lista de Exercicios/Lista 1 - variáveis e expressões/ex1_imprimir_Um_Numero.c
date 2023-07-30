6f2c0ac7ba78ffef856a22ef23393227cad5f817
edvunge <costaedy18@gmail.com>
2023-07-30 21:50:48 +0100

    exercises list 1

---
+++ Lista de Exercicios/Lista 1 - variáveis e expressões/ex1_imprimir_Um_Numero.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+1. Faça um programa que leia um número inteiro e o imprima.
+*/
+int main(void){
+    int num;
+
+    printf("\nNumero: ");
+    scanf("%d",&num);
+
+    printf("numero: %d",num);
+    return 0;
+}
---
+++ Lista de Exercicios/Lista 1 - variáveis e expressões/exercicioTest.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void){
+
+    int idade;
+
+    printf("insira a sua idade");
+    scanf("%d",&idade);
+    /*for(int i = 0; i < 16;i++){
+        printf("\nI: %d",i);
+    }*/
+    printf("a sua idade e: %d",idade);
+    system("pause");
+    return 0;
+}
