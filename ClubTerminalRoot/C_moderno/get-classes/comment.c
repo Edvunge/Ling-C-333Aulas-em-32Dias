8ffafb6ec341d937f8167a8ea87895d1c34dfbba
edvunge <costaedy18@gmail.com>
2024-04-17 23:47:02 +0100

    update

---
+++ ClubTerminalRoot/C_moderno/get-classes/comment.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("## aula de comentarios em C ##\n");
+    
+    // comentario de uma unica linha
+
+    /*
+    comentario 
+    de 
+    multiplas 
+    linhas
+    */ 
+    
+    return (0);
+}
---
+++ ClubTerminalRoot/C_moderno/get-classes/helloWorld.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    
+    printf("Hello curso C moderno!\n");
+    printf("Da Terminal Root!\n");
+    return (0);
+}
