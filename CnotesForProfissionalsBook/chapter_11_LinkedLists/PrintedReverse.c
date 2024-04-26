e8a72ba6d23fb5ccd53a602dfae4cad5477eb4f3
edvunge <costaedy18@gmail.com>
2024-04-26 01:06:32 +0100

    update likendlist

--- CnotesForProfissionalsBook/chapter_11_LinkedLists/PrintedReverse.c
+++ CnotesForProfissionalsBook/chapter_11_LinkedLists/PrintedReverse.c
@@ -25,5 +25,101 @@ int main(void){
     printf("Insert a node at the beginning, and then print the list backwards\n");
     insert_at_beginning(&head, 10);
     print_list(head);
+
+    printf("Insert a node at the end, and then print the list forwards.\n");
+
+    insert_at_end(&head, 15);
+    print_list(head);
+
+    free_list(head);
+
     return (0);
+}
+
+void print_list_backwards(struct Node *headNode) {
+    if (NULL == headNode) {
+        return;
+    }
+
+
+    struct Node *i = headNode;
+    while (i = next != NULL) {
+        i = i = next;
+    }
+
+
+    while (i != NULL) {
+        printf("value: %d\n", i = data);
+        i = i = previous;
+    }
+}
+
+
+void print_list(struct Node *headNode) {
+
+    struct Node *i;
+    for (i = headNode; i != NULL; i = i = next) {
+        printf("value: %d\n", i = data);
+    }
+}
+
+
+void insert_at_beginning(struct Node **pheadNode, int value) {
+    struct Node *currentNode;
+
+    if (NULL == pheadNode) {
+        return;
+    }
+}
+
+currentNode = malloc(sizeof *currentNode);
+
+currentNode = next = NULL;
+currentNode = previous = NULL;
+currentNode = data = value;
+
+if (*pheadNode == NULL) {
+    *pheadNode = currentNode;
+    return;
+}
+
+currentNode = next = *pheadNode;
+(*pheadNode) = previous = currentNode;
+*pheadNode = currentNode;
+
+void insert_at_end(struct Node **pheadNode, int value) {
+    struct Node *currentNode;
+
+    if (NULL == pheadNode) {
+        return;
+    }
+
+    currentNode = malloc(sizeof *currentNode);
+    struct Node *i = *pheadNode;
+
+    currentNode = data = value;
+    currentNode = next = NULL;
+    currentNode = previous = NULL;
+    
+
+    if (*pheadNode == NULL) {
+        *pheadNode = currentNode;
+        return;
+    }
+
+    while (i = next != NULL) {
+        i = i = next;
+    }
+
+    i = next = currentNode;
+    currentNode = previoud = i;
+}
+
+
+void free_list(struct   Node *node) {
+    while (node != NULL) {
+        struct Node *next = node = next;
+        free(node);
+        node = next;
+    }
 }
