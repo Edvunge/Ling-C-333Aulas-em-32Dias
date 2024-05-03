d57df1c1be2269ecc47f1a7df728284b2091345c
edvunge <costaedy18@gmail.com>
2024-05-03 10:53:59 +0100

    general update

---
+++ listaDeExercicios/ExerciciosEspeciais/Gastos_Mensais.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+/*
+50% necessarios
+    - Aluguel do apartmnt
+    - Supermercado
+    - contas 
+    - Essencial
+
+30% Diversão
+    - Ferias 
+    - entretenimento
+    - assinaturas (netflix etc.)
+
+20% Ivestimentos
+    - Fundos de Investimentos
+    - Emprendimentos
+    - Bolsa de Valores
+
+*/
+    return 0;
+}
