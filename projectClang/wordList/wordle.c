3493c79bf890da9b928fb8ab910b3af4d6120f26
Edvaldo vunge <53275378+Edvunge@users.noreply.github.com>
2025-01-14 00:01:40 +0000

    Update wordle.c

--- projectClang/wordList/wordle.c
+++ projectClang/wordList/wordle.c
@@ -4,6 +4,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define ResultGreen    1
+#define ResultYellow   2
+#define ResultRed      4
+
+struct s_result {
+    char color;
+}
+
+Result cc(char,char,int,char*);
+int main();
+
+Result cc(char correct, char guess, int idx, char *word) {
+// git remote set-url origin git@github.com:Edvunge/Ling-C-333Aulas-em-32Dias.git
+
+}
+
 int main(void) {
 
     printf("Hello, World!");
