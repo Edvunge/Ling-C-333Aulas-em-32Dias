8adf2d4417a5040360eb9b128dbf46e04bf62d96
edvaldovunge <costaedy18@gmail.com>
2025-02-04 16:28:03 +0000

    File Encryptor in C

---
+++ aulas-do-youtube/Dr_Jonas_birch/FileEncryptor_inC/fse.h
@@ -0,0 +1,16 @@
+// fse.h
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+//#include <arcfour.h>
+#include "arcfour.h"
+#include <assert.h>
+#include <sys/random.h>
+#include <fcntl.h>
+
+int8 *securerand(int16);
+int8 *readkey(int8*);
+int main(int,char**);
+
