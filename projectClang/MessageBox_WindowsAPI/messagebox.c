ad42a8c685f5ce056a2bedda750f120113724614
edvaldovunge <costaedy18@gmail.com>
2025-02-13 20:18:09 +0000

    message box

---
+++ projectClang/MessageBox_WindowsAPI/messagebox.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+	
+	printf("Hello World\n");
+	int MessageBox(NULL, "If you see this message, you should subscribe!","Ultimate Message Box", MB_YESNOCANCEL);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
