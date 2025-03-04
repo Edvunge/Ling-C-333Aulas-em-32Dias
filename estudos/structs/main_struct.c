f1f96309b37c2f9b200ecae4d813bb50a6127fd8
edvaldovunge <costaedy18@gmail.com>
2025-03-04 19:53:24 +0000

    structs studys

---
+++ estudos/structs/main_struct.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+//
+// Created by Edvaldo Vunge on 04/03/2025.
+// structs study
+
+struct Student {
+  char name[50];
+  char id[10];
+  int age;
+  int grades[5];
+};
+
+int main(void) {
+  struct Student kevin;
+
+  strcpy(kevin.name, "Kevin");
+  strcpy(kevin.id, "000123123");
+  kevin.age = 40;
+  kevin.grades[0] = 1;
+  kevin.grades[1] = 2;
+  kevin.grades[2] = 3;
+  kevin.grades[3] = 4;
+  kevin.grades[4] = 5;
+
+  printf("Name: %s\n", kevin.name);
+  printf("ID: %s\n", kevin.id);
+  printf("Age: %d\n", kevin.age);
+  printf("Grades: ");
+
+  for (int i = 0; i < 5; i++)
+    printf("%d ", kevin.grades[i]);
+  printf("\n");
+  return 0;
+}
