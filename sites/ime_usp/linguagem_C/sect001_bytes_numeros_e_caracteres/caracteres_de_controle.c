bf49d7ffee0b48612e326b4a3f1568afe9c38f46
edvaldovunge <costaedy18@gmail.com>
2025-03-06 21:55:19 +0000

    new project

---
+++ sites/ime_usp/linguagem_C/sect001_bytes_numeros_e_caracteres/caracteres_de_controle.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/*
+// 
+
+\0      caractere nulo(null)
+\t      tabulação horizontal (tab)
+\n      fim de linha (newline)
+\v      tabulação vertical
+\f      fim de pagina (new page)
+\r      carriage return
+
+*/
+
+int main(void) {
+        printf("\\0 caractere nulo(null)\n");
+        printf("\\t tabulação horizontal (tab)\n");
+        printf("\\n fim de linha (newline)\n");
+        printf("\\v tabulação vertical\n");
+        printf("\\f fim de pagina (new page)\n");
+        printf("\\r carriage return\n");
+    return (0);
+}
+
+
+
---
+++ sites/ime_usp/linguagem_C/sect002_tipos_int_e_char/tipo_char.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+    
+    typedef char bbyte = 'a';
+
+    printf("typedef char bbyte: %c\n", bbyte);
+
+    return (0);
+}
