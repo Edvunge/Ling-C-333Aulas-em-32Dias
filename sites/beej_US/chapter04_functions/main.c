18c3acf4e53c958184974d545e3d5b54fa4df6f6
edvaldovunge <costaedy18@gmail.com>
2025-03-12 19:41:29 +0000

    flow control, functions and random exemple

---
+++ sites/beej_US/chapter04_functions/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int plus_one(int n){
+    return n + 1;
+}
+
+int main(void) {
+    
+    int i = 10, j;
+
+    j = plus_one(i);  // The "call" 
+
+    printf("i + 1 is %d\n", j);
+
+    return (0);
+}
+
