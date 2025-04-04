59ebaa35539b40cac4194d0ef832df516d3ba330
edvaldovunge <costaedy18@gmail.com>
2025-04-04 21:41:29 +0100

    exercises

--- exercicios/04_VetoresMatrizes/part1_vetores/Exerc02.c
+++ exercicios/04_VetoresMatrizes/part1_vetores/Exerc02.c
@@ -1,10 +1,20 @@
-#
-#
+#include <stdio.h>
+#include <stdlib.h>
 
 //  Crie um programa que le 6 valores inteiros e, em seguida, 
-// mostre na tela os valores lidos
+//  mostre na tela os valores lidos
 
 int main(void) {
+    const int LE = 6; 
+    int numbers[LE];
 
+    for (int i = 0; i < LE; i++) {
+        printf("Digite %dº do numero: ",i);
+        scanf("%d",&numbers[i]);
+    }
+
+    for (int n = 0; n < LE; n++) {
+        printf("POSITION [%d] = %d\n",n ,numbers[n]);
+    }
     return (0);
 }
---
+++ exercicios/04_VetoresMatrizes/part1_vetores/Exerc05.c
@@ -0,0 +1,3 @@
+
+
+// 5. Leia um vetor de 10 posic ̧  ̃oes. Contar e escrever quantos valores pares ele possui
