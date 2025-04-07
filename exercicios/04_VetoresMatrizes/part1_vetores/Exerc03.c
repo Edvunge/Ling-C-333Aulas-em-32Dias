aa3eff8a2d2bbaa496562bf6f599136db03974d0
edvaldovunge <costaedy18@gmail.com>
2025-04-07 23:20:09 +0100

    exercises

--- exercicios/04_VetoresMatrizes/part1_vetores/Exerc03.c
+++ exercicios/04_VetoresMatrizes/part1_vetores/Exerc03.c
@@ -1,6 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+// Ler um conjunto de numeros reais, 
+// armazenando-o em vetor e calcular o quadrado das
+// componentes deste vetor, armazenando o resultado em outro vetor. 
+// Os conjuntos tem 10 elementos cada. Imprimir todos os conjuntos.
 
-// Ler um conjunto de n  ́umeros reais, armazenando-o em vetor e calcular o quadrado das
-// componentes deste vetor, armazenando o resultado em outro vetor. Os conjuntos t ˆem
-// 10 elementos cada. Imprimir todos os conjuntos.
+int main(void) {
+    const int LENT = 10;
+    double vet[LENT];
+    double tev[LENT];
+
+    for (int y = 0; y < LENT; y++) {
+        printf("Number for Position[%d]: ",y);
+        scanf("%lf", &vet[y]);
+    }
+
+    for (int x = 0; x < LENT; x++) {
+        tev[x] = vet[x]*vet[x];
+    }
+
+    for (int i = 0; i < LENT; i++) {
+        printf(" %.2lf \n", tev[i]);
+    }
+    return (0);
+}
 
--- exercicios/04_VetoresMatrizes/part1_vetores/Exerc04.c
+++ exercicios/04_VetoresMatrizes/part1_vetores/Exerc04.c
@@ -1,5 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+// 4. Faça um programa que leia um vetor de 8 posições e, em seguida, 
+// leia também dois valores X e Y quaisquer correspondentes 
+// a duas posicoes no vetor. 
+// Ao final seu programa deverá escrever a soma dos valores 
+// encontrados nas respectivas posiçoes X e Y.
 
+int main(void) {
+    int vet[8] = {12, 13, 14, 15, 16, 17, 18, 19};
+    int x = vet[2]; 
+    int y = vet[7];
 
-// 4. Fac ̧a um programa que leia um vetor de 8 posic ̧  ̃oes e, em seguida, leia tamb  ́em dois va-
-// lores X e Y quaisquer correspondentes a duas posic ̧  ̃oes no vetor. Ao final seu programa
-// dever  ́a escrever a soma dos valores encontrados nas respectivas posic ̧  ̃oes X e Y .
+    // soma dos valores (x+y)
+    int soma = x + y;
+    printf("Soma vet[x] %d + vet[y] %d = %d ",vet[2] ,vet[7] ,soma);
+    return (0);
+}
