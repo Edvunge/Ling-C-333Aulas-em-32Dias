07f77815cf2462b6b18676d4b1c658ec058a6e6e
edvaldovunge <costaedy18@gmail.com>
2025-06-11 23:52:56 +0100

    metaDay06 – Estruturas de repetição

---
+++ aulas-do-youtube/programe_seu_futuro/3_Estr_de_Repeticao/metaDay06/Aula65_EstruturaDeRepeticao-PARAdecres.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//              Aula 64: Estrutura de Repeticação - PARA
+
+int main(void) {
+    int i = 10;
+
+    for(i = 10; i >= 0; i--)
+        printf(" %d ", i);
+    return (0);
+} 
